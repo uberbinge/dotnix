c56af10d10697393ad178fbdd837bc524fad9f21
uberbinge <1692495+uberbinge@users.noreply.github.com>
2025-10-06 12:53:00 +0200

    update nixvim lsp config to use vim.lsp.config api

--- common/nixvim.nix
+++ common/nixvim.nix
@@ -109,6 +109,65 @@ in
         vim.keymap.set('n', '<leader>sn', function()
           require('telescope.builtin').find_files { cwd = vim.fn.stdpath 'config' }
         end, { desc = '[S]earch [N]eovim files' })
+
+        -- LSP Configuration using new vim.lsp.config API
+        local function setup_lsp_keymaps(bufnr)
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
+          end
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        end
+
+        local function on_attach(client, bufnr)
+          setup_lsp_keymaps(bufnr)
+
+          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = bufnr,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
+            })
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+              buffer = bufnr,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+              callback = function(event)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event.buf }
+              end,
+            })
+          end
+
+          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+            vim.keymap.set('n', '<leader>th', function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr })
+            end, { buffer = bufnr, desc = '[T]oggle Inlay [H]ints' })
+          end
+        end
+
+        -- Configure Lua Language Server using new API
+        vim.lsp.config.lua_ls = {
+          cmd = { 'lua-language-server' },
+          settings = {
+            Lua = {
+              completion = { callSnippet = "Replace" }
+            }
+          },
+          on_attach = on_attach,
+        }
       '';
       globals = {
         mapleader = " ";
@@ -171,59 +230,6 @@ in
             highlight = { enable = true; disable = [ "ruby" ]; };
           };
         };
-        lsp = {
-          enable = true;
-          servers = {
-            lua_ls = {
-              enable = true;
-              settings = {
-                Lua = { completion = { callSnippet = "Replace"; }; };
-              };
-            };
-          };
-          onAttach = ''
-            local map = function(keys, func, desc, mode)
-              mode = mode or 'n'
-              vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
-            end
-            map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-            map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-            map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-            map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-            map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-            map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-            map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-            map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-            map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-            local client = vim.lsp.get_client_by_id(client_id)
-            if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-              local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-              vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                buffer = bufnr,
-                group = highlight_augroup,
-                callback = vim.lsp.buf.document_highlight,
-              })
-              vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                buffer = bufnr,
-                group = highlight_augroup,
-                callback = vim.lsp.buf.clear_references,
-              })
-              vim.api.nvim_create_autocmd('LspDetach', {
-                group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-                callback = function(event)
-                  vim.lsp.buf.clear_references()
-                  vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event.buf }
-                end,
-              })
-            end
-            if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-              map('<leader>th', function()
-                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr })
-              end, '[T]oggle Inlay [H]ints')
-            end
-          '';
-        };
-        lsp-format.enable = true;
         fidget.enable = true;
         cmp = {
           enable = true;
@@ -304,7 +310,6 @@ in
         luvit-meta
         conform-nvim
         mason-nvim
-        mason-lspconfig-nvim
         telescope-fzf-native-nvim
         telescope-ui-select-nvim
       ];
